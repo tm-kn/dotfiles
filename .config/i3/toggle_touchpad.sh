458c265de2c1557cbf842115ad1a75f3bdc24d59
Tomasz Knapik <tomasz.knapik@torchbox.com>
2018-08-03 13:55:02 +0100

    Improve touchpad toggle script

--- .config/i3/toggle_touchpad.sh
+++ .config/i3/toggle_touchpad.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-TOUCHPAD="SynPS/2 Synaptics TouchPad"
+TOUCHPAD=$(xinput list --name-only | grep -i -E "synaptics|touchpad" | head -n 1)
+
+if [ -z "$TOUCHPAD" ]; then
+    >&2 echo "No touchpad found"
+    exit 1
+fi
 
 if [ $(xinput list-props "$TOUCHPAD" | grep "Device Enabled" | grep -o "[01]$") -eq 0 ]; then
     xinput enable "$TOUCHPAD"
