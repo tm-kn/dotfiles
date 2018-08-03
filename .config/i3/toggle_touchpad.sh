9430e65a841d7677c24e225a1bb59465607b0917
Tomasz Knapik <me@tmkn.org>
2018-08-03 18:37:43 +0100

    Support toggling of multiple touchpads

--- .config/i3/toggle_touchpad.sh
+++ .config/i3/toggle_touchpad.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-TOUCHPAD=$(xinput list --name-only | grep -i -E "synaptics|touchpad" | head -n 1)
+inputs=$(xinput list --name-only | grep -i -E "synaptics|touchpad" | head -n 1)
 
-if [ -z "$TOUCHPAD" ]; then
+if [ -z "$inputs" ]; then
     >&2 echo "No touchpad found"
     exit 1
 fi
 
-if [ $(xinput list-props "$TOUCHPAD" | grep "Device Enabled" | grep -o "[01]$") -eq 0 ]; then
-    xinput enable "$TOUCHPAD"
-else
-    xinput disable "$TOUCHPAD"
-fi
+echo $inputs | while read line;
+do
+    if [ $(xinput list-props "$line" | grep "Device Enabled" | grep -o "[01]$") -eq 0 ]; then
+        xinput enable "$line"
+    else
+        xinput disable "$line"
+    fi
+done
