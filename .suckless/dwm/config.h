d1cff6b2c0ab15383fdba782762e319fc0e9af21
Tomasz Knapik <hi@tmkn.org>
2018-10-18 01:52:27 +0100

    Update dwm config

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 0,       0,           -1 },
 	{ "URxvt",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Slack",  NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "discord",  NULL,       NULL,       1 << 6,       0,           -1 },
